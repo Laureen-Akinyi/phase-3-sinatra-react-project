0c51e5e96f7de29047bb83c578168f49cd150c97
laureen <laureen.akinyi@student.moringaschool.com>
2022-09-12 13:29:15 +0300

    added patients values in patients table

--- db/migrate/20220912102627_create_patients.rb
+++ db/migrate/20220912102627_create_patients.rb
@@ -1,4 +1,13 @@
 class CreatePatients < ActiveRecord::Migration[6.1]
   def change
+    create_table :patients do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :age
+      t.integer :weight
+      t.integer :phone
+      t.string :kin
+      t.string :drug_admin
+    end
   end
 end
