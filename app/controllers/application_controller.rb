6c47b3da6e60560f8f8213b169485d4a6e846265
laureen <laureen.akinyi@student.moringaschool.com>
2022-09-12 19:20:20 +0300

    changes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
   # Add your routes here
-  get "/patients" do
+  get '/patients' do
     patients = Patient.all.order(:created_at)
     patients.to_json
   end
