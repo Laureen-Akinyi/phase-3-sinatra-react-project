c281d7b09a23747411c0326bfa8d8815c04a28b7
laureen <laureen.akinyi@student.moringaschool.com>
2022-09-12 13:36:18 +0300

    added values in the doctors table

--- db/migrate/20220912103354_create_doctors.rb
+++ db/migrate/20220912103354_create_doctors.rb
@@ -1,4 +1,10 @@
 class CreateDoctors < ActiveRecord::Migration[6.1]
   def change
+    create_table :doctors do |t|
+      t.string :first_name
+      t.string :last_name
+      t.integer :phone
+      t.string :department
+    end 
   end
 end
